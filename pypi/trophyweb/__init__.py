b24cdb2acd4a34f5cc575cccb95c1074f38a7c48
Hugo Wainwright <hugowainwright@fb.com>
2018-11-04 20:59:55 +0000

    removed node, moved to babylon

--- pypi/trophyweb/__init__.py
+++
@@ -1,8 +0,0 @@
-import flask
-
-app = flask.Flask(__name__, static_url_path="")
-
-
-@app.route("/")
-def redirect_to_index():
-    return flask.redirect(flask.url_for(redirect_to_index.__name__) + "index.html", code=301)
